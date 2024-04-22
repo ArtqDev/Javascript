2d9dca261bce3be63b56ccfd8cf752d7a0a3c680
Arthur Quintino <artdevelopernew@gmail.com>
2024-04-22 01:16:52 -0300

    iniciando estudos em JS

---
+++ 01/index.js
@@ -0,0 +1,6 @@
+console.log('ola');
+
+//  window.alert('bora ganbada de viado');
+
+document.getElementById("titulo").textContent = "TITULO";
+document.getElementById("paragrafo").textContent = "lorem";
